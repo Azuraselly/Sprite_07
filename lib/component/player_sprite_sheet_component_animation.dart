7d0fd36d8dd275a8ce305c835dab8e6592bc0b44
Azuraselly <azuraselly224@gmail.com>
2025-09-18 21:05:10 +0700

    challenge 1

--- lib/component/player_sprite_sheet_component_animation.dart
+++ lib/component/player_sprite_sheet_component_animation.dart
@@ -1,12 +1,12 @@
-import 'package:flame/sprite.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
 import 'package:flame/components.dart';
 import 'package:sprite_07/utils/create_animation_by_limit.dart';
 
 class PlayerSpriteSheetComponentAnimation extends SpriteAnimationComponent with HasGameReference {
   late double screenWidth;
   late double screenHeight;
-
+  
   late double centerX;
   late double centerY;
 
@@ -16,28 +16,35 @@ class PlayerSpriteSheetComponentAnimation extends SpriteAnimationComponent with 
   late SpriteAnimation dinoAnimation;
 
   @override
-  void onLoad() async {
+  void onLoad()async {
     super.onLoad();
+
     screenWidth = game.size.x;
     screenHeight = game.size.y;
-    centerX = (screenWidth/2) - (spriteSheetWidth/2);
-    centerY = (screenHeight/2) - (spriteSheetHeight/2);
+
+    centerX = (screenWidth / 2) - (spriteSheetWidth / 2);
+    centerY = (screenHeight / 2) - (spriteSheetHeight / 2);
 
     var spriteImages = await Flame.images.load('dino.png');
 
     final spriteSheet = SpriteSheet(
       image: spriteImages,
-      srcSize: Vector2(spriteSheetWidth, spriteSheetHeight));
-      position=Vector2(centerX, centerY);
-      size=Vector2(spriteSheetWidth, spriteSheetHeight);
+      srcSize: Vector2(spriteSheetWidth, spriteSheetHeight),
+    );
+
+    position = Vector2(centerX, centerY);
+    size = Vector2(spriteSheetWidth, spriteSheetHeight);
 
     dinoAnimation = spriteSheet.createAnimationByLimit(
       xInit: 0,
       yInit: 0,
-      step: 10,
-      sizeX: 2,
-      stepTime: .80,
-    );
+      step: 8,
+      sizeX: 8,
+      stepTime: 0.1,
+      loop: true,
+    );  
+
     animation = dinoAnimation;
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flame/game.dart';
+import 'package:flame/events.dart'; 
 import 'package:flutter/material.dart';
-import 'package:sprite_07/component/player_sprite_sheet_component_animation.dart';
+import 'package:sprite_07/component/player_sprite_sheet_component_animation_full.dart';
 
-class MyGame extends FlameGame {
+
+class MyGame extends FlameGame with HasKeyboardHandlerComponents {
   @override
-  void onLoad() async {
+  void onLoad() {
     super.onLoad();
-    add(PlayerSpriteSheetComponentAnimation());
+    add(PlayerSpriteSheetComponentAnimationFull());
   }
 }
 
-void main() async {
+void main() async{
   runApp(GameWidget(game: MyGame()));
 }
